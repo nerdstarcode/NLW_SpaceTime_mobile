a5f7c04635d68d3acae0b2b1817e148625a93ace
sthiven <sthiven.correia@global.ntt>
2023-05-18 14:42:14 -0300

    :construction: mobile, token mockado porque to no pc da empresa e to tendo problema com as rotas no android studio kk

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native';
-
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import * as SecureStore from 'expo-secure-store';
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
 
 const StyledStripes = styled(Stripes)
@@ -18,13 +18,62 @@ import {
 import {
   BaiJamjuree_700Bold
 } from '@expo-google-fonts/bai-jamjuree'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+import { api } from '../src/lib/api';
+import { useRouter } from 'expo-router';
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold
   })
+
+  // const discovery = {
+  //   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  //   tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  //   revocationEndpoint: 'https://github.com/settings/connections/applications/91ac5b6bfd37af1ee687',
+  // };
+  // const [request, response, sigInWithGithub] = useAuthRequest(
+  //   {
+  //     clientId: '91ac5b6bfd37af1ee687',
+  //     scopes: ['identity'],
+  //     redirectUri: makeRedirectUri({
+  //       scheme: 'nlwspacetime'
+  //     }),
+  //   },
+  //   discovery
+  // );
+  async function handleGithubOAuthCode(code: string) {
+    // const response = await api.post('/register', {
+    //   code,
+    // })
+
+    // const { token } = response.data
+
+    await SecureStore.setItemAsync('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiU3RoaXZlbiBNZWxvIiwiYXZhdGFyVXJsIjoiaHR0cHM6Ly9hdmF0YXJzLmdpdGh1YnVzZXJjb250ZW50LmNvbS91Lzk1MzEyNTQzP3Y9NCIsInN1YiI6IjljMmNlNTUyLWE3MmUtNDk4ZC1iZDFjLTBmMzM3ZmEyZTA1NCIsImlhdCI6MTY4NDQyMTYwNywiZXhwIjoxNjg3MDEzNjA3fQ.uSqNCaZMuX5YNk88Ck2o8PUgJ5j-s1oZvc8lp0qkurw')
+
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   'response',
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    // if (response?.type === 'success') {
+    // const { code } = response.params
+    handleGithubOAuthCode('code')
+    // }
+    // }, [response]);
+  }, []);
   if (!hasLoadedFonts) {
     return null
   }
@@ -50,6 +99,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className='rounded-full bg-green-500 px-5 py-2'
+          onPress={() => {/*sigInWithGithub()*/ }}
         >
           <Text className='font-alt text-sm uppercase text-black leadint'>Cadastrar lembrança</Text>
         </TouchableOpacity>
